df4917d08cb00aacf26f28f1e8572dad96c1ed33
HR <justindequillo>
2020-10-02 06:36:15 +0800

    'changes-10-2-20'

---
+++ pages/components/nav.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Link from 'next/link'
+
+const Nav = () =>{
+	return(
+		<>	
+			<nav className="main-nav">
+				<ul>
+				  	<li>
+			        	<Link href="/">
+				          <a>Home</a>
+				        </Link>
+				    </li>
+				    <li>
+				        <Link href="/about">
+				          <a>About Us</a>
+				        </Link>
+				    </li>
+				    <li>
+				        <Link href="/blog">
+				          <a>Blog Post</a>
+				        </Link>
+				    </li>
+				</ul>
+			</nav>	
+		</>
+	)
+}
+
+export default Nav;
